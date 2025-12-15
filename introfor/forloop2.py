7473e103bae877e992c2b78ab9664a658e72e26c
Sovereign-V <louis.brewer13@gmail.com>
2025-12-15 22:36:09 +0000

    modifying logic

--- introfor/forloop2.py
+++ introfor/forloop2.py
@@ -5,7 +5,7 @@
 # create a list of strings
 vendors = ["cisco", "juniper", "big_ip", "f5", "arista", "alta3", "zach", "stuart"]
 # create a second list of strings
-approved_vendors = ["cisco", "juniper", "big_ip"]
+approved_vendors = input("vendor?")  
 # loop across the list called vendors
 for x in vendors:
     print("\nThe vendor is " + x, end="")   # newline, print current vendor, and end without newline
